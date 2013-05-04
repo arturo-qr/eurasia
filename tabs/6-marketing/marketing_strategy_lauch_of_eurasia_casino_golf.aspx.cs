9bdbe3b230b6d6dd92d4ebf6f45ba7267154df98
Juan Rocandio <global.rocandio@gmail.com>
2013-05-04 13:56:55 -0700

    Marketing tab and images

---
+++ tabs/6-marketing/marketing_strategy_lauch_of_eurasia_casino_golf.aspx.cs
@@ -0,0 +1,1 @@
+using System;using System.Collections.Generic;using System.Linq;using System.Web;using System.Web.UI;using System.Web.UI.WebControls;public partial class marketing_strategy_lauch_of_eurasia_casino_golf : System.Web.UI.Page{    protected void Page_Load(object sender, EventArgs e)    {    }}
